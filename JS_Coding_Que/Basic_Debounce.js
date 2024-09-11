87e93637476f6d94c305b76baea51ea2cf70f5b1
unknown <satul388@gmail.com>
2024-09-11 21:31:02 +0530

    first commit2

--- JS_Coding_Que/Basic_Debounce.js
+++ JS_Coding_Que/Basic_Debounce.js
@@ -1,4 +1,4 @@
-Debounce is a common technique used in Web Application, in most cases using lodash solution would be a good choice.
+/* Debounce is a common technique used in Web Application, in most cases using lodash solution would be a good choice.
 
 could you implement your own version of basic debounce()?
 
@@ -17,3 +17,29 @@ notes
 please follow above spec. the behavior might not be exactly the same as lodash.debounce()
 
 because window.setTimeout and window.clearTimeout are not accurate in browser environment, they are replaced to other implementation when judging your code. They still have the same interface, and internally keep track of the timing for testing purpose.
+
+*/
+
+
+// This is a JavaScript coding problem from BFE.dev 
+
+/**
+ * @param {(...args: any[]) => any} func
+ * @param {number} wait
+ * @returns {(...args: any[]) => any}
+ */
+function debounce(func, wait) {
+    let timeId = null;
+    return function(...args) {
+        if(timeId) {
+          clearTimeout(timeId);
+        }
+      timeId = setTimeout(() => {
+        func(...args);
+        timeId = null;
+        }, wait);
+    }
+  }
+  
+  
+  
