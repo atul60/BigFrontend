a0184afb76ebeb2bf8f4df718b16231fedd94d09
unknown <satul388@gmail.com>
2024-09-15 09:51:33 +0530

    Data Type

---
+++ JS_Coding_Que/Data_type.js
@@ -0,0 +1,27 @@
+/* 
+This is an easy problem.
+
+For all the basic data types in JavaScript, how could you write a function to detect the type of arbitrary data?
+
+Besides basic types, you need to also handle also commonly used complex data type including Array, ArrayBuffer, Map, Set, Date and Function
+
+The goal is not to list up all the data types but to show us how to solve the problem when we need to.
+
+The type should be lowercase
+
+detectType(1) // 'number'
+detectType(new Map()) // 'map'
+detectType([]) // 'array'
+detectType(null) // 'null'
+// more in judging step
+*/
+
+
+/**
+ * @param {any} data
+ * @return {string}
+ */
+function detectType(data) {
+    if(data instanceof FileReader) return 'object'
+    return Object.prototype.toString.call(data).slice(1, -1).split(' ')[1].toLowerCase();
+  }
