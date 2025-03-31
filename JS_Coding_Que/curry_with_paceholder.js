a1956a62ef55eb28c582fdb7e381762131aae0f7
unknown <satul388@gmail.com>
2025-03-31 18:18:08 +0530

    new changes

--- JS_Coding_Que/curry_with_paceholder.js
+++ JS_Coding_Que/curry_with_paceholder.js
@@ -14,34 +14,35 @@ curriedJoin(_, 2)(1, 3) // '1_2_3'
 curriedJoin(_, _, _)(1)(_, 3)(2) // '1_2_3' */
 
 const join = (a, b, c) => {
-    return `${a}_${b}_${c}`
- }
+  return `${a}_${b}_${c}`;
+};
 
- function curry(fn) {
-    const intermediateFn = function(...args) {
-      const validArgs = getValidArgs(...args);
-      if(validArgs.length >= 3) return fn(...validArgs);
-      return function(...nextArgs) {
-        return intermediateFn(...nextArgs, ...args);
-      }
-    }
-  
-    return intermediateFn;
-  }
-  
-  const getValidArgs = (...args) => {
-    return args.filter((arg) => typeof arg == 'number');
-  }
-  
-  
-  curry.placeholder = Symbol()
+// This is a JavaScript coding problem from BFE.dev
 
-  const curriedJoin = curry(join)
-const _ = curry.placeholder
-// let ans = curriedJoin(1, 2, 3) // '1_2_3'
-let ans = curriedJoin(_, 2)(1, 3) // '1_2_3'
-// curriedJoin(_, _, _)(1)(_, 3)(2)
+/**
+ * @param { (...args: any[]) => any } fn
+ * @returns { (...args: any[]) => any }
+ */
+function curry(fn) {
+  const intermediateFn = function (...args) {
+    const validArgs = getValidArgs(...args);
+    let sortedArgs = validArgs.sort();
+    if (sortedArgs.length >= 3) return fn(...sortedArgs);
+    return function (...nextArgs) {
+      return intermediateFn(...sortedArgs, ...nextArgs);
+    };
+  };
 
-console.log('ans: ', ans);
+  return intermediateFn;
+}
 
+const getValidArgs = (...args) => {
+  return args.filter((arg) => typeof arg == "number");
+};
 
+curry.placeholder = Symbol();
+
+/* Failed Test Cases
+(_,_,3,4)(1,_)(2,5) spec  TypeError: curry(...)(...)(...) is not a function
+
+*/
