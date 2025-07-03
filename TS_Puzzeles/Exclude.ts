642f2ddb27db7ba4fabfd0ef9b57718c8b40ee77
Atul <74779836+atul60@users.noreply.github.com>
2025-07-03 12:42:19 +0000

    MyExclude

--- TS_Puzzeles/Exclude.ts
+++ TS_Puzzeles/Exclude.ts
@@ -1,1 +1,7 @@
+/* Implement the built-in Exclude<T, U>
+
+Exclude from T those types that are assignable to U */
+
+type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+
 type MyExclude<T, E> = T extends E ? never : T
