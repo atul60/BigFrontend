f8941664a9e1742101db8c6506201082d1398ff4
Atul <74779836+atul60@users.noreply.github.com>
2025-07-04 09:13:03 +0000

    MyOmit

--- TS_Puzzeles/Omit.ts
+++ TS_Puzzeles/Omit.ts
@@ -1,3 +1,17 @@
-type MyOmit<T, K extends keyof any> = {
-    [key in keyof T as key extends K ? never : key] : T[key]
-  }
+/* Implement the built-in Omit<T, K> generic without using it.
+
+Constructs a type by picking all properties from T and then removing K */
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyOmit<Todo, "description" | "title">;
+
+const todo: TodoPreview = {
+  completed: false,
+};
+type MyOmit<T, K extends keyof T> = {
+  [key in keyof T as key extends K ? never : key]: T[key];
+};
