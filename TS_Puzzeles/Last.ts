2d47eb4455cd77ecb99d947011f657661160344e
Atul <74779836+atul60@users.noreply.github.com>
2025-07-07 10:58:12 +0000

    Last

--- TS_Puzzeles/Last.ts
+++ TS_Puzzeles/Last.ts
@@ -1,6 +1,6 @@
 /* Implement a generic Last<T> that takes an Array T and returns its last element. */
 type arr1 = ["a", "b", "c"];
-type arr2 = [3, 2, 1];
+type arr2 = [3, 2, 1, 0];
 
 type tail1 = Last<arr1>; // expected to be 'c'
 type tail2 = Last<arr2>; // expected to be 1
